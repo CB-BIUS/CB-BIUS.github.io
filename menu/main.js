c6ec6af4d87e84c910e76fad3f139e90c5bc3fe7
CB-BIUS <67938171+CB-BIUS@users.noreply.github.com>
2020-12-22 15:51:59 -0500

    Update main.js

--- menu/main.js
+++ menu/main.js
@@ -4,6 +4,6 @@ function myFunction() {
 }
 
 function submitForm(form) {
-    window.open("mailto:cambaffuto@gmail.com?subject=Dinner%20Suggestion&body=Hi%20my%20name%20is%20" + form.name.value + "%0D%0A" + "%20I%20would%20like...%20" + form.comment.value);
+    window.open("mailto:"Menu Admin"<cambaffuto@gmail.com?subject=Dinner%20Suggestion&body=Hi%20my%20name%20is%20" + form.name.value + "%0D%0A" + "%20I%20would%20like...%20" + form.comment.value)>;
     return false;
 }
